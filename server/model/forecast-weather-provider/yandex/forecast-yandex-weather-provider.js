4c13f9d53fc006c08948f2a63bf13f4cc5342112
bartlby1987 <bartlby1987@gmail.com>
2020-08-18 15:42:12 +0300

    Issue #13: Refactoring yandex file.Main function structure has been changed.

--- server/model/forecast-weather-provider/yandex/forecast-yandex-weather-provider.js
+++ server/model/forecast-weather-provider/yandex/forecast-yandex-weather-provider.js
@@ -2,7 +2,7 @@ const request = require('sync-request');
 const cheerio = require('cheerio');
 const forecastWeatherUtil = require("../forecast-weather-utilities");
 const MAIN_PAGE_URL = `https://yandex.ru/pogoda/search?request=`;
-const DETAIL_PAGE_URL = "https://yandex.ru/pogoda/details?";
+const FORECAST_PAGE_URL = "https://yandex.ru/pogoda/details?";
 const HREF_ATR_SELECTOR = "body > div > div> div> div > div> div > li:nth-child(1) > a";
 const generateDayTempSelector = (value) => `body > div.b-page__container > div.content > div.forecast-details-segment > div` +
     `> div > div:nth-child(${value}) > dd > table > tbody > tr:nth-child(1) >` +
@@ -21,53 +21,73 @@ const generateHumidityNightSelector = (value) => `body > div.b-page__container >
 const FIRST_DAY_VALUE = 3;
 const SECOND_DAY_VALUE = 4;
 const THIRD_DAY_VALUE = 5;
-const INDEX_FOR_REPLACE = "/pogoda/?";
+const PART_URL_FOR_REPLACEMENT = "/pogoda/?";
 const mappingProvider = [{"dayValue": FIRST_DAY_VALUE}, {"dayValue": SECOND_DAY_VALUE}, {"dayValue": THIRD_DAY_VALUE}];
-function getForecastWeather(city) {
-    let mainPageUrl = encodeURI(MAIN_PAGE_URL + city);
-    let res = request('GET', mainPageUrl, {});
-    let htmlDataPage = cheerio.load(res.body);
-    let href = htmlDataPage(HREF_ATR_SELECTOR).attr("href");
-    href = href.replace(INDEX_FOR_REPLACE, "");
-    let detailPageUrl = DETAIL_PAGE_URL + href;
-    detailPageUrl = encodeURI(detailPageUrl);
-    let rest = request('GET', detailPageUrl, {});
-    let htmlDetailPage = cheerio.load(rest.body);
 
-    function getTempDay(dayValue) {
-        let tempDay = htmlDetailPage(generateDayTempSelector(dayValue)).text();
-        if (tempDay === "") {
-            return undefined
-        } else {
-            return tempDay
-        }
+function getTempDay(dayValue, forecastPageHtml) {
+    let tempDay = forecastPageHtml(generateDayTempSelector(dayValue)).text();
+    if (tempDay === "") {
+        return undefined
+    } else {
+        return tempDay
     }
+}
 
-    function getTempNight(dayValue) {
-        let tempNight = htmlDetailPage(generateNightTempSelector(dayValue)).text();
-        if (tempNight === "") {
-            return undefined
-        } else {
-            return tempNight
-        }
+function getTempNight(dayValue, forecastPageHtml) {
+    let tempNight = forecastPageHtml(generateNightTempSelector(dayValue)).text();
+    if (tempNight === "") {
+        return undefined
+    } else {
+        return tempNight
     }
+}
 
-    function getHumidityDay(dayValue) {
-        return htmlDetailPage(generateHumidityDaySelector(dayValue)).text();
-    }
+function getHumidityDay(dayValue, forecastPageHtml) {
+    return forecastPageHtml(generateHumidityDaySelector(dayValue)).text();
+}
 
-    function getHumidityNight(dayValue) {
-        return htmlDetailPage(generateHumidityNightSelector(dayValue)).text();
-    }
+function getHumidityNight(dayValue, forecastPageHtml) {
+    return forecastPageHtml(generateHumidityNightSelector(dayValue)).text();
+}
 
+function constructSearchPageUrlByCity(city) {
+    return encodeURI(MAIN_PAGE_URL + city);
+}
+
+function extractSearchPageHtmlByCity(searchPageUrl) {
+    let res = request('GET', searchPageUrl, {});
+    return cheerio.load(res.body);
+}
+
+function constructForecastPageUrlByCity(searchPageHtml) {
+    let href = searchPageHtml(HREF_ATR_SELECTOR).attr("href");
+    href = href.replace(PART_URL_FOR_REPLACEMENT, "");
+    let forecastPageUrl = FORECAST_PAGE_URL + href;
+    return encodeURI(forecastPageUrl);
+}
+
+function extractForecastPageHtmlByCity(forecastPageUrl) {
+    let rest = request('GET', forecastPageUrl, {});
+    return cheerio.load(rest.body);
+}
+
+function parseForecastPageHtmlByCity(forecastPageHtml) {
     return mappingProvider.map((element) => {
         return {
-            tempDay: Number(getTempDay(element.dayValue)),
-            tempNight: Number(getTempNight(element.dayValue)),
-            humidityDay: forecastWeatherUtil.getTransformHumidity(getHumidityDay(element.dayValue)),
-            humidityNight: forecastWeatherUtil.getTransformHumidity(getHumidityNight(element.dayValue))
+            tempDay: Number(getTempDay(element.dayValue, forecastPageHtml)),
+            tempNight: Number(getTempNight(element.dayValue, forecastPageHtml)),
+            humidityDay: forecastWeatherUtil.getTransformHumidity(getHumidityDay(element.dayValue, forecastPageHtml)),
+            humidityNight: forecastWeatherUtil.getTransformHumidity(getHumidityNight(element.dayValue, forecastPageHtml))
         }
     })
+}
+
+function getForecastWeather(city) {
+    let searchPageUrl = constructSearchPageUrlByCity(city);
+    let searchPageHtml = extractSearchPageHtmlByCity(searchPageUrl);
+    let forecastPageUrl = constructForecastPageUrlByCity(searchPageHtml);
+    let forecastPageHtml = extractForecastPageHtmlByCity(forecastPageUrl);
+    return parseForecastPageHtmlByCity(forecastPageHtml);
 }
 
 module.exports = {
